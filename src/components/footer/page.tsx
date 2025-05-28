387404986c038cf126102728e2ef3c259794843e
muslum <muslumdogan01@gmail.com>
2025-05-28 23:54:11 +0100

    home complated

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Link from "next/link";
 import { SocialIcon } from "./socialIcon";
+import { usePathname } from "next/navigation";
 
 import FooterLogo from "./logo";
 import LogoTitle from "./logoTitle";
@@ -7,9 +10,10 @@ import LogoLine from "./logoLine";
 import Image from "next/image";
 
 export default function Footer() {
+  const pathname = usePathname();
   return (
-    <footer className="w-full bg-black text-white pt-[110px] ">
-      <div className="2xl:max-w-[1440px] mx-auto pb-[69px] px-4 flex flex-col md:flex-row md:items-center md:justify-between">
+    <footer className="w-full bg-black text-white pt-[50px] lg:pt-[110px] ">
+      <div className="2xl:max-w-[1440px] mx-auto pb-[50px] lg:pb-[69px] px-4 flex flex-col md:flex-row md:items-center md:justify-between">
         <div className="flex">
           {/* Logo - hidden on mobile */}
           <div className="hidden md:flex items-center">
@@ -20,47 +24,54 @@ export default function Footer() {
               </Link>
             </div>
           </div>
-          <div className="px-6 w-fit flex items-center">
+          <div className="px-6 w-fit hidden md:flex  items-center">
             <LogoLine />
           </div>
           {/* Navigation - vertical on mobile, horizontal on desktop */}
-          <nav className="flex flex-col items-center space-y-4 md:flex-row md:space-y-0 md:space-x-6 md:items-center">
-            <Link href="/" className="hover:text-gray-300 transition-colors">
+          <nav className="flex flex-col  items-center justify-center w-full space-y-4 md:flex-row md:space-y-0 md:space-x-6 ">
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? "text-white" : "text-[#7A7A7A]"
+              } hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]`}
+            >
               Home
             </Link>
             <Link
               href="/features"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className={`${
+                pathname === "/features" ? "text-white" : "text-[#7A7A7A]"
+              } hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]`}
             >
               Features
             </Link>
             <Link
               href="/integrations"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className="text-[#7A7A7A] hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]"
             >
               Integrations
             </Link>
             <Link
               href="/pricing"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className="text-[#7A7A7A] hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]"
             >
               Pricing
             </Link>
             <Link
               href="/insight"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className="text-[#7A7A7A] hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]"
             >
               Insight
             </Link>
             <Link
               href="/faq"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className="text-[#7A7A7A] hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]"
             >
               F.A.Q
             </Link>
             <Link
               href="/contact"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
+              className="text-[#7A7A7A] hover:text-[#949494] font-inter text-[14px] not-italic font-semibold leading-[140%]"
             >
               Contact
             </Link>
@@ -68,7 +79,7 @@ export default function Footer() {
         </div>
 
         {/* Social icons */}
-        <div className="flex justify-center mt-6 md:mt-0 space-x-4">
+        <div className="flex justify-center mt-[50px] md:mt-0 space-x-4">
           <Link href="#" className="hover:opacity-80 transition-opacity">
             <SocialIcon name="Facebook" />
             <span className="sr-only">Facebook</span>
@@ -87,13 +98,22 @@ export default function Footer() {
           </Link>
         </div>
       </div>
-      <div className="w-full bg-white/10 min-h-[325px] pt-[60px]">
+      <div className="w-full flex justify-center lg:hidden">
+        <div className="bg-white/20 w-full h-[1px] mx-5 mb-[50px] lg:mb-0"></div>
+      </div>
+      <div className="w-full lg:bg-white/10 lg:min-h-[325px] mb-[50px] lg:mb-0 lg:pt-[60px]">
         <div className="2xl:max-w-[1440px] mx-auto">
-          <div className="w-full flex justify-between items-center px-4">
-            <h1 className="text-white text-[14px] not-italic font-normal leading-[140%]">© 2024 WareAct. All rights reserved</h1>
-            <div className="flex space-x-[40px] text-white text-[14px] not-italic font-normal leading-[140%]">
-              <span>Privacy Policy</span>
-              <span>Terms and Condition</span>
+          <div className="w-full flex flex-col lg:flex-row lg:justify-between items-center mb-[50px] lg:mb-0 px-4">
+            <h1 className="hidden lg:block text-white text-[14px] not-italic font-normal leading-[140%]">
+              © 2024 WareAct. All rights reserved
+            </h1>
+            <div className="flex space-x-[40px] text-white/30 lg:text-white text-[14px] not-italic font-normal leading-[140%]">
+              <span className="cursor-pointer hover:text-gray-300">
+                Privacy Policy
+              </span>
+              <span className="cursor-pointer hover:text-gray-300">
+                Terms and Condition
+              </span>
             </div>
           </div>
         </div>
--- src/components/signup/page.tsx
+++ src/components/signup/page.tsx
@@ -10,24 +10,24 @@ export default function SignupCard() {
   }
 
   return (
-    <div className="flex items-center justify-center px-4  mb-60">
-      <div className="w-[361px] h-[370px] lg:w-[1224px] lg:h-[255px] rounded-[30px] bg-blue-500 lg:bg-transparent lg:bg-[url('/icons/signup/bg.svg')]  lg:bg-no-repeat lg:bg-cover  flex flex-col">
+    <div className="flex items-center justify-center px-4  lg:mb-60">
+      <div className="w-[361px] h-[370px] lg:w-[1224px] lg:h-[235px] mb-[100px] lg:mb-0 2xl:h-[255px] rounded-[30px] bg-blue-500 lg:bg-transparent lg:bg-[url('/icons/signup/bg.svg')]  lg:bg-no-repeat lg:bg-cover  flex flex-col">
         <div className="flex-1 w-full flex flex-col lg:flex-row justify-center  lg:justify-evenly  lg:items-center">
           <h2 className="text-white text-3xl font-normal text-center lg:text-left mb-[30px] leading-[1.3]">
             Take the first step <br className="hidden lg:block" /> to create your account
           </h2>
 
-          <div className="space-y-5 lg:flex ">
+          <div className="space-y-5 lg:space-y-0 flex flex-col lg:flex-row justify-center items-center">
             <input
               type="email"
               placeholder="Enter your email"
               value={email}
               onChange={handleEmailChange}
-              className="w-full lg:w-[392px] h-14 lg:h-[70px] rounded-[10px] lg:rounded-br-none lg:rounded-tr-none bg-white border-none text-black/40 font-inter text-base not-italic font-normal leading-[1.36] pl-5 focus:outline-none focus:ring-0 focus:border-transparent"
+              className="w-[329px] 2xl:w-[392px] h-14 lg:h-[70px] rounded-[10px] lg:rounded-br-none lg:rounded-tr-none bg-white border-none text-black/40 font-inter text-base not-italic font-normal leading-[1.36] pl-5 focus:outline-none focus:ring-0 focus:border-transparent"
             />
 
             <button
-              className="w-full lg:w-[104px] h-14 lg:h-[70px]  bg-black hover:bg-black/90 text-white rounded-[10px] lg:rounded-bl-none lg:rounded-tl-none  font-medium transition-colors"
+              className="w-[329px]  lg:w-[104px] h-14 lg:h-[70px]  bg-black hover:bg-black/90 text-white rounded-[10px] lg:rounded-bl-none lg:rounded-tl-none  font-medium transition-colors"
               type="button"
             >
               Start
