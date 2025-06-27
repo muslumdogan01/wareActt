396bb1c3bb488e3a04a4c111acd91ca192e06cc4
muslum <muslumdogan01@gmail.com>
2025-06-28 00:36:19 +0100

    bugs

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -188,7 +188,7 @@ const SignUpForm: React.FC = () => {
   );
 
   return (
-    <div className="min-h-screen max-h-screen overflow-y-auto flex items-center justify-center bg-gray-100 ">
+    <div className="h-auto flex justify-center m-5 bg-gray-100 ">
       <form
         onSubmit={handleSubmit}
         className="w-full max-w-[490px] bg-white p-[50px] rounded-2xl shadow-lg space-y-4 flex flex-col justify-center items-center"
