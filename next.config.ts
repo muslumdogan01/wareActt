c242b1dc2d17735379f9841f400d37dfef3cce0a
muslum <muslumdogan01@gmail.com>
2025-09-05 17:04:54 +0100

    ffggf

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
- 
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'prized-excitement-8827b8b6f4.strapiapp.com',
-        pathname: '/uploads/**', 
-      },
-      {
-        protocol: 'https',
-        hostname: 'prized-excitement-8827b8b6f4.media.strapiapp.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        pathname: '/**',
-      },
+    domains: [
+      'localhost',
+      'prized-excitement-8827b8b6f4.strapiapp.com',
+      'prized-excitement-8827b8b6f4.media.strapiapp.com', 
     ],
   },
 };
-
 module.exports = nextConfig;
